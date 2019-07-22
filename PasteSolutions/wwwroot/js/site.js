4e45667f9381e924a31fa6572c86a464ba332aa2
Izu <rannick10@tutanota.com>
2019-07-22 21:34:56 +0200

    refactoring; added tabbing and back-tabbing in textarea

--- PasteSolutions/wwwroot/js/site.js
+++ PasteSolutions/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿window.postContent = function(str, lang) {
+    var req = new XMLHttpRequest();
+    return new Promise(function(resolve, reject) {
+        req.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    var resJson = JSON.parse(this.response);
+                    resolve(resJson);
+                }
+                else {
+                    var resJson = JSON.parse(this.response);
+                    reject(resJson);
+                }
+            }
+        }
 
-// Write your JavaScript code.
+        req.open("POST", "api/upload", true);
+        req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+
+        var jsonPayload = {
+            snippet: str,
+            language: lang
+        };
+
+        req.send(JSON.stringify(jsonPayload));
+    });
+}
+
+var lastHighlight;
+
+window.highlightSelectedLine = function() {
+    var hash = window.location.hash;
+    if (!hash) return;
+
+    if (lastHighlight)
+        $(lastHighlight).removeClass('selected');
+
+    var number = hash.substr(1, hash.length-1);
+    lastHighlight = $(`.line-number #${number}`).first().parent();
+    lastHighlight.addClass('selected');
+}
